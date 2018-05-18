ee2396c631fd812e79388beecc429a1cf8d0188d
glennm95 <glennmascarenhas24395@gmail.com>
2018-05-17 20:06:56 -0500

    Submitted version of Project 1

---
+++ Project1/app/src/main/java/edu/uic/cs478/gmasca2/project1/MainActivity.java
@@ -0,0 +1,62 @@
+package edu.uic.cs478.gmasca2.project1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void firstButtonPressed(View view) {
+        Intent intent = new Intent(this, PhoneInput.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, final Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                Button button = findViewById(R.id.button2);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Bundle extras = data.getExtras();
+                        String userInputCorrect = extras.getString("keyName");
+
+                        Uri number = Uri.parse("tel:" + userInputCorrect);
+                        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                        startActivity(callIntent);
+                    }
+                });
+            }
+            else if (resultCode == RESULT_CANCELED)
+            {
+                Button button = (Button) findViewById(R.id.button2);
+                button.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        Bundle extras = data.getExtras();
+                        String userInputWrong = extras.getString("keyName");
+
+                        Toast.makeText(getApplicationContext(), "You entered an incorrect number: " + userInputWrong, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Project1/app/src/main/java/edu/uic/cs478/gmasca2/project1/PhoneInput.java
@@ -0,0 +1,49 @@
+package edu.uic.cs478.gmasca2.project1;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class PhoneInput extends Activity {
+    public EditText input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_input);
+
+        input = findViewById(R.id.editText2);
+
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener () {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if((event!=null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    String s = input.getText().toString();
+
+                    Intent intent = getIntent();
+                    intent.putExtra("keyName", s);
+
+                    Pattern p = Pattern.compile("\\s*\\([0-9][0-9][0-9]\\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]\\s*");
+                    Matcher m = p.matcher(s);
+                    if(m.matches())
+                        setResult(RESULT_OK, intent);
+                    else
+                        setResult(RESULT_CANCELED, intent);
+                    finish();
+                }
+                return false;
+            }
+        });
+    }
+
+}
+
